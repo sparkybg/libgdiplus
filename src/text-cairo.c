17410ef2f6bc1efe07c0aa595aadc1bfe7666bff
Spartak Timchev <sparky@omegatim.com>
2021-06-09 00:59:13 +0300

    aaa

--- src/text-cairo.c
+++ src/text-cairo.c
@@ -241,9 +241,9 @@ MeasureString (GpGraphics *graphics, GDIPCONST WCHAR *stringUnicode, int *length
 		}
 	}
 
-	//cairo_font_options_set_antialias(FontOptions, CAIRO_ANTIALIAS_NONE);
-	//cairo_font_options_set_hint_style(FontOptions, CAIRO_HINT_STYLE_NONE);
-	//cairo_font_options_set_hint_metrics(FontOptions, CAIRO_HINT_METRICS_OFF);
+	cairo_font_options_set_antialias(FontOptions, CAIRO_ANTIALIAS_NONE);
+	cairo_font_options_set_hint_style(FontOptions, CAIRO_HINT_STYLE_NONE);
+	cairo_font_options_set_hint_metrics(FontOptions, CAIRO_HINT_METRICS_OFF);
 	((mycairo_font_options*)FontOptions)->round_glyph_positions = CAIRO_ROUND_GLYPH_POS_OFF;
 
 	cairo_set_font_options(graphics->ct, FontOptions);
